8c4d9ade9892c777e52f3478f833259b81e9a9ce
Cleiton Ferreira <cleitonrferreira@gmail.com>
2023-02-16 23:02:47 -0300

    Remove ignored files

--- src/main/java/io/github/frcleiton/medicin/model/repository/MedicamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.frcleiton.medicin.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.github.frcleiton.medicin.model.entity.Medicamento;
-
-public interface MedicamentoRepository extends JpaRepository<Medicamento, Integer>{
-
-}
